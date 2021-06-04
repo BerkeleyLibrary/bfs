a371c2d9e3f2a23515c7e75a869ee76d14b0c0a9
David Zuckerman <davidzuckerman@Davids-iMac.local>
2021-06-04 09:49:02 -0700

    took out commented out code

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -7,9 +7,7 @@ do
   #for file in $dir/in_dir/*; do
     input_file="$(basename ${file})"
     ruby /opt/app-root/src/src/invoice_parser.rb $input_file 
-    #ruby /opt/app-root/src/src/invoice_parser.rb "${file##*/}"
-    #echo "${file##*/}"
-    echo $input_file
+    
     sleep 2
   done
   
