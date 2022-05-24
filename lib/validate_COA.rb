b8f1c5cce19015ce9dc46204ae792f9b76ee2594
David Zuckerman <davidzuckerman@Davids-iMac.local>
2022-05-24 10:31:32 -0700

    took out credentials

--- lib/validate_COA.rb
+++ lib/validate_COA.rb
@@ -4,7 +4,6 @@ require 'nokogiri'
 
 def self.get_response(chart_string)
   HTTParty.post('https://apis-qa.berkeley.edu/coa',query: {'COA' => chart_string},
-  #  headers: {'app_id' => @app_id ,'app_key' => @app_key, 'content-type' => 'application/xml'}
     headers: {'app_id' => ENV['COA_APP_ID'] ,'app_key' => ENV['COA_APP_KEY'], 'content-type' => 'application/xml'}
   )
 end
@@ -14,8 +13,5 @@ def self.get_status(chart_string)
   xml = Nokogiri::XML(results.to_s) 
   xml.xpath("//ValidateCOAResponse/UC_COA_CHK/COA/StatusText1").text
 end
-
-#@app_id = '6debf8ae'
-#@app_key =  '20e083c2336f047476040f73bc3d2111'
 
 end
